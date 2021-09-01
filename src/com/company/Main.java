9907ed7328c73937db10ab20bf2813f46a5d086a
Izzet <izzetates85@gmail.com>
2021-09-01 12:59:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+
+        for (int i= 0;i<intList.size();i++){
+            System.out.println(i + " " + intList.get(i));
+        }
+
+        intList.add(1,2);
+
+        for (int i= 0;i<intList.size();i++){
+            System.out.println(i + " " + intList.get(i));
+        }
+
+
+    }
+}
